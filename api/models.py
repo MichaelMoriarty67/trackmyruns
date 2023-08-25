1e54efa0df9b77cdec77ecffa8dcb14425124ba5
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2023-08-24 17:34:57 -0700

    second commit lol

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Run(models.Model):
+    date_pub = models.DateTimeField("publish date")
+    duration_kms = models.FloatField(default=0)
+    duration_sec = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.date_pub.year}/{self.date_pub.month}/{self.date_pub.day} | {self.duration_kms}km | {self.duration_sec / 60} min"
