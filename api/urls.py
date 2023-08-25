6668752b51402684104d06938c100f77b38f3ffd
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2023-08-24 20:28:13 -0700

    learning Aug24th

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "api"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
+    path("run_by_id/<int:run_id>", views.runs_by_id, name="name_by_id"),
+    path("new/<int:run_id>", views.create_run, name="create run"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Run
+from django.utils import timezone
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("yooo, an api is gonna be here quick fr fr.")
+    runs = Run.objects.order_by("-date_pub")
+    output = [r.duration_kms for r in runs]
+
+    return HttpResponse(f"Your longest runs: {output}")
+
+
+def runs_by_id(request, run_id):
+    response = f"Here are the details for the run with id {run_id}"
+    return HttpResponse(response)
+
+
+def create_run(request, run_id):
+    new_run = Run(date_pub=timezone.now(), duration_kms=69.420, duration_sec=run_id)
+    new_run.save()
+
+    return HttpResponse(f"You're creating a run: {new_run}")
